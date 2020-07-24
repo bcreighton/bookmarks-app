53adbf20de9333a5376064d8e76b79ba2233effe
Bryson Creighton <brysoncreighton@gmail.com>
2020-07-24 12:32:41 -0600

    Cont dev
    developing edit component

--- src/EditBookmark/EditBookmark.js
+++ src/EditBookmark/EditBookmark.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import BookmarksContext from '../BookmarksContext'
 import config from '../config'
 import './EditBookmark.css'
@@ -11,6 +12,7 @@ class EditBookmark extends Component {
   static contextType = BookmarksContext
 
   state = {
+    id: '',
     title: '',
     url: '',
     description: '',
@@ -18,39 +20,71 @@ class EditBookmark extends Component {
     error: null,
   }
 
-  handleSubmit = () => { }
+  handleChangeTitle = e => {
+    this.setState({ title: e.target.value })
+  }
+
+  handleChangeUrl = e => {
+    this.setState({ url: e.target.value })
+  }
+  handleChangeDescription = e => {
+    this.setState({ description: e.target.value })
+  }
+  handleChangeRating = e => {
+    this.setState({ rating: e.target.value })
+  }
+
+  handleSubmit = () => {
+    e.preventDefault()
+
+    const { bookmarkId } = this.props.match.params
+    const { id, title, url, description, rating } = this.state
+    const newBookmark = { id, title, url, description, rating }
+
+    fetch(confg.API_ENDPOINT + `/${bookmarkId}`, {
+      method: 'PATCH',
+      body: JSON.stringify(newBookmark),
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `Bearer ${config.API_KEY}`
+      }
+    })
+      .then(res => {
+        if (!res.ok)
+          return res.json().then(error => Promise.reject(error))
+      })
+  }
 
   componentDidMount() {
     const bookmarkId = this.props.match.params.bookmarkId
 
-    fetch(`https://localhost:8000/api/bookmark/${bookmarkId}`, {
-      method: 'GET'
+    fetch(config.API_ENDPOINT + `${bookmarkId}`, {
+      method: 'GET',
+      headers: {
+        'authorization': `Bearer ${config.API_KEY}`
+      }
     })
       .then(res => {
-        if (!res.ok) {
+        if (!res.ok)
           // get the error message from the response,
-          return res.json().then(error => {
-            // then throw it
-            throw error
-          })
-        }
+          return res.json().then(error => Promise.reject(error))
 
         return res.json()
       })
       .then(responseData => {
-        // this.setState({
-        //   title,
-        //   url,
-        //   description,
-        //   rating
-        // })
-        debugger;
+        this.setState({
+          id: responseData.id,
+          title: responseData.title,
+          url: responseData.url,
+          description: responseData.description,
+          rating: responseData.rating,
+        })
       })
       .catch(error => {
         this.setState({ error })
       })
-
   }
+
 
   render() {
     const { error } = this.state
